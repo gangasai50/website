02b62c3353efec497313c1006d230da6b2f3f0cb
gangasai50 <gangasai@uskcorp.in>
2024-09-03 17:36:00 +0530

    removed phone validation

--- send.php
+++ send.php
@@ -88,11 +88,11 @@ if ($_POST['phone'] != '') {
 	$_POST['phone'] = filter_var($_POST['phone'], FILTER_SANITIZE_STRING);
 
 	// After sanitization validation is performed
-	$pattern_phone = array('options'=>array('regexp'=>'/^\+{1}[0-9]+$/'));
-	$_POST['phone'] = filter_var($_POST['phone'], FILTER_VALIDATE_REGEXP, $pattern_phone);
+	//$pattern_phone = array('options'=>array('regexp'=>'/^\[0-9]+$/'));
+	//$_POST['phone'] = filter_var($_POST['phone'], FILTER_VALIDATE_REGEXP, $pattern_phone);
 	
 	if($_POST['phone'] == '') {
-		$errors .= 'Please enter a valid phone number like: +363012345<br/>';
+		$errors .= 'Please enter a valid phone number like: 363012345<br/>';
 	}
 }
 
